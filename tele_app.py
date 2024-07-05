46a1ac29e10294181ad6ad7e9dd74f09d656b7e4
Mike Fluff <mf@mikefluff.com>
2024-07-05 14:03:12 +0700

    Update tele_app.py

--- tele_app.py
+++ tele_app.py
@@ -2,6 +2,7 @@ import os
 from fastapi import FastAPI, HTTPException, Depends, UploadFile, File
 from fastapi.staticfiles import StaticFiles
 from telethon import TelegramClient, functions, types
+from telethon.sessions import StringSession
 from telethon.errors import SessionPasswordNeededError, PhoneCodeInvalidError
 from pydantic import BaseModel
 from contextlib import asynccontextmanager
