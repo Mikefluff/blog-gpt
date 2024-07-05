b23cfd6816c183bd2f0ab02200963f6f0c777768
Mike Fluff <mf@mikefluff.com>
2024-07-05 14:58:44 +0700

    Update tele_app.py

--- tele_app.py
+++ tele_app.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, HTTPException, Depends, UploadFile, File
+from fastapi import FastAPI, HTTPException, Depends, Body, UploadFile, File
 from fastapi.staticfiles import StaticFiles
 from telethon import TelegramClient, functions, types
 from telethon.sessions import StringSession
